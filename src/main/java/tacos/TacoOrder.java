932531bfce67a4d82bd6767d7dbc315a7b190491
OReoN_03 <gorkov.artem03@mail.ru>
2023-09-18 08:46:24 +0300

    Added spring data jdbc support

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -1,5 +1,6 @@
 package tacos;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
@@ -9,12 +10,17 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
-public class TacoOrder {
+@Table
+public class TacoOrder implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
     private Long id;
     private Date placedAt;
+
     @NotBlank(message = "Delivery name is required")
     private String deliveryName;
     @NotBlank(message = "Street is required")
@@ -25,6 +31,7 @@ public class TacoOrder {
     private String deliveryState;
     @NotBlank(message = "Zip code is required")
     private String deliveryZip;
+
     @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
     @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message = "Must be formatted MM/YY")
