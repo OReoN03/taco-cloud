23bcdd5d57e48f36892f116a75c610aebbecd98a
OReoN_03 <gorkov.artem03@mail.ru>
2023-10-01 09:55:08 +0300

    Using mongodb for data storage

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -12,17 +12,20 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document
 @Table("orders")
 public class TacoOrder implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @PrimaryKey
-    private UUID id = Uuids.timeBased();
+    @Id
+    private String id;
 
     private Date placedAt = new Date();
 
@@ -50,7 +53,6 @@ public class TacoOrder implements Serializable {
     @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
 
-    @Column("tacos")
     private List<TacoUDT> tacos = new ArrayList<>();
 
     public void addTaco(TacoUDT taco) {
